454a3e0b2100cd3f702ef2dd565d11ae30bb8d4e
Andrei Constantin <andrei.constantin@outlook.com>
2024-12-16 19:25:55 +0000

    Moved matplotlib code from main_new to separate file

--- src/main_new.py
+++ src/main_new.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
-import os
 import repository_utils
 import matplotlib.pyplot as plt
 from models.CustomCommit import CustomCommit
 from models.JavaFileHandler import JavaFileHandler
 from models.LanguageFileHandler import LanguageFileHandler
+import graphs
 
 def retrieve_files(modified_files, file_handler: LanguageFileHandler):
     """
@@ -105,34 +105,6 @@ def find_nearest_implementation(test_file, commits, commit_map, file_handler: La
     return before_index if distance_before <= distance_after else after_index
 
 
-def plot_bar_graph(test_before, test_during, test_after, repo):
-    """
-    Function to plot a bar chart showing the order of commits for test files
-    @param test_before: An Integer representing the number of tests that were committed BEFORE their corresponding implementation
-    @param test_during: An Integer representing the number of tests that were committed TOGETHER with their corresponding implementation
-    @param test_after: An Integer representing the number of tests that were committed AFTER their corresponding implementation
-    @param repo: A String representing the repository which we will search and retrieve commits from
-    """
-    # Specify the bars names
-    categories = ['Test Before Impl.', 'Same Time', 'Impl. Before Test']
-    # Match the values for each bar
-    values = [test_before, test_during, test_after]
-    # Give each bar a different color
-    plt.bar(categories, values, color=["#1984c5", "#22a7f0", "#63bff0"])
-    # Assign the plot a title, x, and y label
-    plt.title('Test and Implementation commit order. Repo: ' + repo)
-    plt.xlabel('Test/Implementation Commit Order')
-    plt.ylabel('Number of Test/Implementation Pairs')
-    # Basic Formatting
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    # Save the plot
-    output_dir = "./../plots/"
-    os.makedirs(output_dir, exist_ok=True)
-    plt.savefig(output_dir + repo + "_plot.jpg")
-    
-    plt.show()
-
 def main():
     # Use repository_utils to get an array from the list of allowed repositories
     repositories = repository_utils.read_repository_names("java")[1:2]
@@ -176,17 +148,7 @@ def main():
         print("Test during Implementation: " + str(test_during))
 
         # Plot the bar graph using the function
-        plot_bar_graph(test_before, test_during, test_after, repo.split("/")[-1].split(".")[0])
+        graphs.plot_bar_graph(test_before, test_during, test_after, repo.split("/")[-1].split(".")[0])
 
 
 main()
-
-
-'''
-WE MUST LOOK AT COMMITS
-'''
-
-
-'''
-COMPACT occurrences where a test file has multiple instances in different commits
-'''
