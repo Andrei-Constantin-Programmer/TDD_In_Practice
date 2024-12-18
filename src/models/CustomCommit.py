14ce56948c603ba3b8d246fdb2a594a3751af9e5
Nishil <nishil.enq@gmail.com>
2024-12-18 16:39:42 +0000

    synced up main and authors_processing to start on extensions to main, also added size to customcommit.py

--- src/models/CustomCommit.py
+++ src/models/CustomCommit.py
@@ -4,9 +4,11 @@ class CustomCommit:
         self.modified_files = modified_files
         self.author = author
         self.date = date
+        self.size = len(self.modified_files)
 
     def __str__(self):
         return ("\nCOMMIT - " + self.hash +
                 "\nMODIFIED - " + str(self.modified_files) +
                 "\nAUTHOR - " + str(self.author) +
-                "\nDATE - " + str(self.date) + "\n")
+                "\nDATE - " + str(self.date) + "\n" +
+                "\nSIZE - " + str(self.size))
