2a82d97dce8cc997f64b3d4f47f0cf296f5d8358
malmousawi <96029001+malmousawi@users.noreply.github.com>
2024-12-30 22:54:45 +0000

    Ran unqie repo script to test it works

--- src/unique_repos.py
+++ src/unique_repos.py
@@ -14,8 +14,8 @@ def remove_duplicate_lines(input_file, output_file):
         writer = csv.writer(outfile)
         writer.writerows(unique_lines)
 
-input_csv = "/repos_to_analyse/repos.csv"
-output_csv = "/repos_to_analyse/unique_repos.csv"
+input_csv = "./repos_to_analyse/repos.csv"
+output_csv = "./repos_to_analyse/unique_repos.csv"
 remove_duplicate_lines(input_csv, output_csv)
 
 print(f"Unique lines have been written to {output_csv}")
